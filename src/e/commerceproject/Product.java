06e40e0af44d6116c2d399d8abb2a35b8708f37d
vietnq02 <vietnguyenquoc089@gmail.com>
2026-06-19 16:42:50 +0700

    List All Products
    created Product.java and ProductManager.java

---
+++ src/e/commerceproject/Product.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package e.commerceproject;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+/**
+ *
+ * @author Admin
+ */
+public class Product {
+
+    private String id;
+    private String name;
+    private double price;
+    private int rating;
+
+    public Product(String id, String name, double price, int rating) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    @Override
+    public String toString() {
+        NumberFormat vn = NumberFormat.getInstance(new Locale("vi", "VN"));
+        return String.format("| %-5s | %-20s | %15s ₫ | %d/5 |",
+                id, name, vn.format(price), rating);
+    }
+}
---
+++ src/e/commerceproject/ProductManager.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package e.commerceproject;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Admin
+ */
+public class ProductManager {
+
+    private ArrayList<Product> products;
+
+    public ProductManager() {
+        products = new ArrayList<>();
+        loadSampleData();
+    }
+
+    private void loadSampleData() {
+        products.add(new Product("P01", "iPhone 17", 24990000, 5));
+        products.add(new Product("P02", "Keyboard", 500000, 4));
+        products.add(new Product("P03", "Mouse", 250000, 3));
+        products.add(new Product("P04", "Monitor", 6500000, 4));
+        products.add(new Product("P05", "Headphone", 1200000, 5));
+        products.add(new Product("P06", "Laptop", 25990000, 5));
+        products.add(new Product("P07", "USB Hub", 200000, 3));
+        products.add(new Product("P08", "Webcam", 900000, 4));
+        products.add(new Product("P09", "Speaker", 1500000, 4));
+        products.add(new Product("P10", "Gaming Chair", 4500000, 3));
+    }
+
+    public void displayProducts() {
+        System.out.println("+-------+----------------------+-----------+------+");
+        System.out.println("| ID    | Name                 |     Price | Rating |");
+        System.out.println("+-------+----------------------+-----------+------+");
+        for (Product p : products) {
+            System.out.println(p);
+        }
+        System.out.println("+-------+----------------------+-----------+------+");
+        System.out.println("Total: " + products.size() + " product(s).");
+    }
+}
